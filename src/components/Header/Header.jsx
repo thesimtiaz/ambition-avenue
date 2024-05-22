fe73495e327a03b6b5b9e02874f5aa813816897d
thesImtiaz <thesimtiaz@gmail.com>
2024-05-22 17:29:22 +0600

    header section added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,61 @@
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  const menu = (
+    <>
+      <li>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/statistics"}>Statistics</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/applied"}>Applied Jobs</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/blog"}>Blog</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="bg-base-100">
+      <div className="navbar container mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {menu}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-2xl">Ambition Avenue</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{menu}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Star Applying</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
